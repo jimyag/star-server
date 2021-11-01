f701be7bbe8dca090b0f894b81d2c0cc306bbb82
jimyag <jimyag@126.com>
2021-11-01 20:23:37 +0800

    feat: 加入管理员权限控制

--- routes/router.go
+++ routes/router.go
@@ -19,34 +19,21 @@ func InitRouter() {
 	{
 		//用户模块
 		authV1.PUT("user/:uid/update", v1.UpdateUser)
-		authV1.PUT("user/:uid/updateauth", v1.UpdateUserAuth)
 		authV1.GET("user/:uid", v1.GetUser)
 
-		// 紧急通知
-		authV1.POST("notice/add", v1.CreateNotice)
-
-		// 文章
-		authV1.POST("article/add", v1.CreateArticle)
-
 		//部门
-		authV1.POST("sector/add", v1.CreateSector)
 		authV1.POST("sector/:uid/join", v1.CreateStuSect)
 		authV1.GET("sector/:uid/find", v1.FindStuSector)
 		//学生
-		authV1.POST("student/add", v1.CreateStudent)
 		authV1.GET("student/:student_id", v1.GetStudent)
 		//值班表
 		authV1.GET("schedule/:sector_name", v1.GetSchedule)
-		authV1.POST("schedule/add", v1.AddOneRecord)
 
 		// 工作记录
 		authV1.POST("workform/add", v1.CreateForm)
 		authV1.PUT("workform/update/:uid", v1.UpdateForm)
 		authV1.GET("workform/get/:student_id", v1.GetStuForm)
 		// todo 获得用户的工作情况
-
-		//部门邀请码
-		authV1.POST("sectorkey/add", v1.CreateSectorKey)
 
 	}
 	routerV1 := r.Group("api/v1")
@@ -61,7 +48,25 @@ func InitRouter() {
 		routerV1.GET("sector", v1.GetSector)
 
 	}
+	adminV1 := r.Group("api/v1")
+	adminV1.Use(middleware.AdminToken())
+	{
+		// 紧急通知
+		authV1.POST("notice/add", v1.CreateNotice)
+		// 文章
+		authV1.POST("article/add", v1.CreateArticle)
+		//部门
+		authV1.POST("sector/add", v1.CreateSector)
+		// 学生
+		authV1.POST("student/add", v1.CreateStudent)
+		// 一条值班记录
+		authV1.POST("schedule/add", v1.AddOneRecord)
+		//部门邀请码
+		authV1.POST("sectorkey/add", v1.CreateSectorKey)
+		// 更新用户权限
+		authV1.PUT("user/:uid/updateauth", v1.UpdateUserAuth)
 
+	}
 	err := r.Run(utils.HttpPort)
 	if err != nil {
 		return
