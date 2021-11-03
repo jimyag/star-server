e64cc6c3738f77687ec2f65014095315715972f6
jimyag <jimyag@126.com>
2021-11-03 09:38:14 +0800

    feat: 取消配置中的默认属性

--- utils/setting.go
+++ utils/setting.go
@@ -16,13 +16,14 @@ var (
 	DbUser     string
 	DbPassWord string
 	DbName     string
+	DbTimeZone string
 
 	AgentAppid  string
 	AgentSecret string
 )
 
 func init() {
-	file, err := ini.Load("config/config.ini")
+	file, err := ini.Load("config/releaseConfig.ini")
 	if err != nil {
 		fmt.Println("配置文件读取错误，请检查路径")
 	}
@@ -32,21 +33,22 @@ func init() {
 }
 
 func LoadServer(file *ini.File) {
-	AppMode = file.Section("server").Key("AppMode").MustString("debug")
-	HttpPort = file.Section("server").Key("HttpPort").MustString(":3000")
-	JwtKey = file.Section("server").Key("JwtKey").MustString("bdfskd983usdhf03")
+	AppMode = file.Section("server").Key("AppMode").String()
+	HttpPort = file.Section("server").Key("HttpPort").String()
+	JwtKey = file.Section("server").Key("JwtKey").String()
 }
 
 func LoadData(file *ini.File) {
-	Db = file.Section("database").Key("Db").MustString("mysql")
-	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
-	DbPort = file.Section("database").Key("DbPort").MustString("3306")
-	DbUser = file.Section("database").Key("DbUser").MustString("root")
-	DbPassWord = file.Section("database").Key("DbPassWord").MustString("523672645")
-	DbName = file.Section("database").Key("DbName").MustString("star")
+	Db = file.Section("database").Key("Db").String()
+	DbHost = file.Section("database").Key("DbHost").String()
+	DbPort = file.Section("database").Key("DbPort").String()
+	DbUser = file.Section("database").Key("DbUser").String()
+	DbPassWord = file.Section("database").Key("DbPassWord").String()
+	DbName = file.Section("database").Key("DbName").String()
+	DbTimeZone = file.Section("database").Key("DbTimeZone").String()
 }
 
 func LoadAgent(file *ini.File) {
-	AgentAppid = file.Section("agent").Key("Appid  ").MustString("wx5bbeae76ae17f1da")
-	AgentSecret = file.Section("agent").Key("Secret ").MustString("2c8e552595f7ec4331661e7fde1425a1")
+	AgentAppid = file.Section("agent").Key("Appid  ").String()
+	AgentSecret = file.Section("agent").Key("Secret ").String()
 }
