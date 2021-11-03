2f730d238d80fa5bbd7972acf2e6e3105b249a9f
jimyag <jimyag@126.com>
2021-11-03 09:40:03 +0800

    feat: 更换数据库

--- model/db.go
+++ model/db.go
@@ -2,30 +2,29 @@ package model
 
 import (
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+
 	"star-server/utils"
-	"time"
 )
 
 var db *gorm.DB
 var err error
 
 func InitDb() {
-	db, err = gorm.Open(utils.Db, fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		utils.DbUser,
-		utils.DbPassWord,
-		utils.DbHost,
-		utils.DbPort,
-		utils.DbName))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s TimeZone=%s", utils.DbHost, utils.DbUser, utils.DbPassWord, utils.DbName, utils.DbPort, utils.DbTimeZone)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+		NamingStrategy:         schema.NamingStrategy{SingularTable: true},
+	})
 	if err != nil {
 		fmt.Println("连接数据库失败，请检查参数", err)
+		return
 	}
-
-	// 禁用默认表的复数形式
-	db.SingularTable(true)
 	// 数据库的自动迁移
-	db.AutoMigrate(&User{},
+	err = db.AutoMigrate(&User{},
 		&Notice{},
 		&Article{},
 		&Schedule{},
@@ -34,15 +33,24 @@ func InitDb() {
 		&WorkForm{},
 		&Authentication{},
 		&SectorKey{},
-		&StuSector{})
+		&StuSector{},
+		&Mytest{})
+	if err != nil {
+		fmt.Println("数据库创建失败", err)
+		return
+	}
 
 	// 设置连接池中最大的闲置连接数
-	db.DB().SetMaxIdleConns(10)
+	//a, err = db.DB()
+	//a.SetMaxIdleConns(10)
+	//db.DB().SetMaxIdleConns(10)
 
 	// 设置数据库的最大连接数
-	db.DB().SetMaxOpenConns(100)
+	//a.SetMaxOpenConns(100)
+	//db.DB().SetMaxOpenConns(100)
 
 	// 设置连接的最大可复用时间
-	db.DB().SetConnMaxLifetime(10 * time.Second)
+	//a.SetConnMaxLifetime(10 * time.Second)
+	//db.DB().SetConnMaxLifetime(10 * time.Second)
 
 }
