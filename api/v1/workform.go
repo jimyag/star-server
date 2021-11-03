6050cd624aff86e68099d25b3ed3fc2aa04887f5
jimyag <jimyag@126.com>
2021-11-03 21:40:01 +0800

    fix: 增加验证更新记录

--- api/v1/workform.go
+++ api/v1/workform.go
@@ -6,6 +6,7 @@ import (
 	"star-server/utils"
 	"star-server/utils/errmsg"
 	"strconv"
+	"time"
 )
 
 func CreateForm(context *gin.Context) {
@@ -20,19 +21,37 @@ func CreateForm(context *gin.Context) {
 		utils.ResponseOk(context, errmsg.StudentNotExist)
 		return
 	}
-	utils.ResponseDataOk(context, model.CreateForm(&data), data)
+	data.BeginTime = int(time.Now().Unix())
+	code = model.CreateForm(&data)
+	utils.ResponseDataOk(context, code, data)
 }
 
 func UpdateForm(ctx *gin.Context) {
 	var data model.WorkForm
-	id, _ := strconv.Atoi(ctx.Param("id"))
+	uid, _ := strconv.Atoi(ctx.Param("uid"))
 	_ = ctx.ShouldBindJSON(&data)
-	utils.ResponseOk(ctx, model.UpdateForm(id, &data))
+
+	stu_sector, code := model.FindStuSectorUseUid(model.StuSector{Uid: uid})
+	if code == errmsg.ERROR {
+		utils.ResponseOk(ctx, errmsg.StudentNotExist)
+		return
+	}
+	if stu_sector.SectorName != data.SectorName {
+		utils.ResponseOk(ctx, errmsg.StudentNotExist)
+		return
+	}
+	_, code = model.GetFormUseId(data.ID)
+	if code == errmsg.ERROR {
+		utils.ResponseMsgOk(ctx, errmsg.ERROR, "这条工作记录错误")
+		return
+	}
+	data.EndTime = int(time.Now().Unix())
+	utils.ResponseOk(ctx, model.UpdateForm(&data))
 
 }
 
 func GetStuForm(context *gin.Context) {
 	var studentId = context.Param("student_id")
-	forms, err := model.GetForm(studentId)
+	forms, err := model.GetForms(studentId)
 	utils.ResponseDataOk(context, err, forms)
 }
