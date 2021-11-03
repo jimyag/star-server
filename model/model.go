34d5b5f1796a41e45ca88a4092f5d9834fd2432c
jimyag <jimyag@126.com>
2021-11-03 12:49:23 +0800

    feat: 增加自己的model

---
+++ model/model.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Model struct {
+	ID        uint           `gorm:"primarykey"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
